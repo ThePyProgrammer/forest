5a773a15d7d69100febf1a386eb50ee8d09417d6
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:23:46 +0800

    Update __init__.py

--- stdlib/__init__.py
+++ stdlib/__init__.py
@@ -11,5 +11,11 @@ from .iopy import *
 ## shell
 from .shell import *
 
+inspect = LazyImport("import inspect", "LIBRARY: inspect — Inspect live objects, derived from 'import inspect', part of The Python Standard Library")
+
+
 ### debugging
 pdb = LazyImport("import pdb", "LIBRARY pdb — The Python Debugger, derived from 'import pdb', from The Python Standard Library")
+
+### Windows
+wincl = LazyImport('import win32com.client as wincl', "PACKAGE wincl derived from win32com.client — Python for Windows (pywin32) Extensions, derived from 'import win32com.client as wincl', from https://github.com/mhammond/pywin32, Documentation: http://timgolden.me.uk/pywin32-docs/contents.html")
