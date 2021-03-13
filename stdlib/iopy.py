00ac35f60db70f87078a0e3941595cd6fe6ffd0f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:46:38 +0800

    Create iopy.py

---
+++ stdlib/iopy.py
@@ -0,0 +1,8 @@
+## io
+from forest._importable import LazyImport
+
+io = LazyImport("import io", "LIBRARY: io — Core tools for working with streams, derived from 'import io', part of The Python Standard Library")
+glob = LazyImport("import glob", "LIBRARY: glob — Unix style pathname pattern expansion, derived from 'import glob', part of The Python Standard Library")
+Path = LazyImport("from pathlib import Path", "CLASS: pathlib.Path — represents concrete paths of the system’s path flavour, derived from 'from pathlib import Path', part of The Python Standard Library")
+pathlib = LazyImport("import pathlib", "LIBRARY: pathlib — Object-oriented filesystem paths, derived from 'import pathlib', part of The Python Standard Library")
+pickle = LazyImport("import pickle", "LIBRARY: pickle — Python object serialization, derived from 'import pickle', part of The Python Standard Library")
