3e87eb002d6248f749cb1ce05b4a0993f8162863
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 23:47:46 +0800

    Create __init__.py

---
+++ web/__init__.py
@@ -0,0 +1,41 @@
+# web
+from ._importable import LazyImport, _import_statements
+
+
+## url
+urllib = LazyImport("import urllib", "LIBRARY urllib — URL handling modules, derived from 'import urllib', from The Python Standard Library")
+request = LazyImport("from urllib import request", "LIBRARY urllib.request — Extensive library for opening URLs, derived from 'from urllib import request', from The Python Standard Library")
+urllib2 = LazyImport("import urllib.request as urllib2", "LIBRARY urllib2 derived from urllib.request — Extensive library for opening URLs, derived from 'import urllib.request as urllib2', from The Python Standard Library")
+urlopen = LazyImport("from urllib.request import urlopen", "FUNCTION urllib.request.urlopen(url...) — Opens the URL url which can be either a string or a Request object, derived from 'from urllib.request import urlopen', from The Python Standard Library")
+
+
+## scrape
+requests = LazyImport("import requests", "PACKAGE requests — A simple, yet elegant HTTP library, derived from 'import requests', from https://github.com/psf/requests, Documentation: https://requests.readthedocs.io/en/master/")
+BeautifulSoup = LazyImport("from bs4 import BeautifulSoup", "CLASS bs4.BeautifulSoup — HTML/XML Formatter on Python, derived from 'from bs4 import BeautifulSoup', from https://www.crummy.com/software/BeautifulSoup/, Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/")
+scrapy = LazyImport("import scrapy", "PACKAGE scrapy — A fast high-level web crawling & scraping framework for Python, derived from 'import scrapy', from https://github.com/scrapy/scrapy, Documentation: https://docs.scrapy.org/en/latest/index.html")
+
+
+## apis
+newspaper = LazyImport("import newspaper", "PACKAGE newspaper — News, full-text, and article metadata extraction in Python 3, derived from 'import newspaper', from https://github.com/codelucas/newspaper, Documentation: https://newspaper.readthedocs.io/en/latest/")
+Article = LazyImport("from newspaper import Article", "CLASS newspaper.Article — Article scraping and curation, derived from 'from newspaper import Article', from https://github.com/codelucas/newspaper, Documentation: https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html#news-articles")
+fulltext = LazyImport("fromn newspaper import fulltext", "FUNCTION newspaper.fulltext — Extract full text from an article, derived from 'from newspaper import fulltext', from https://github.com/codelucas/newspaper, Documentation: https://newspaper.readthedocs.io/en/latest/")
+wikipedia = LazyImport("import wikipedia", "PACKAGE wikipedia — A Pythonic wrapper for the Wikipedia API, derived from 'import wikipedia', from https://github.com/goldsmith/Wikipedia, Documentation: https://wikipedia.readthedocs.io/en/latest/")
+
+
+## email
+smtplib = LazyImport("import smtplib", "LIBRARY smtplib — SMTP protocol client, derived from 'import smtplib', part of The Python Standard Library")
+
+
+## browser
+webbrowser = LazyImport("import webbrowser", "LIBRARY webbrowser — Convenient Web-browser controller, derived from 'import webbrowser', from The Python Standard Library")
+wb = LazyImport("import webbrowser as wb", "LIBRARY wb derived from webbrowser — Convenient Web-browser controller, derived from 'import webbrowser as wb', from The Python Standard Library")
+openweb = LazyImport("from webbrowser import open as openweb", "FUNCTION openweb derived from webbrowser.open — Display url using the new default browser, derived from 'from webbrowser import open as openweb', from The Python Standard Library")
+
+
+## frameworks
+fastapi = LazyImport("import fastapi")
+FastAPI = LazyImport("from fastapi import FastAPI")
+pydantic = LazyImport("import pydantic")
+tornado = LazyImport("import tornado")
+sanic = LazyImport("import sanic")
+Sanic = LazyImport("from sanic import Sanic")
