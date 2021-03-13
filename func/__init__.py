a354e4cd6d07ea7913a767b6d81fa512328b2835
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:19:54 +0800

    Create __init__.py

---
+++ func/__init__.py
@@ -0,0 +1,6 @@
+### func
+from forest._importable import LazyImport
+
+
+functools = LazyImport("import functools", "LIBRARY: functools — Higher-order functions and operations on callable objects, derived from 'import functools', part of The Python Standard Library")
+typing = LazyImport("import typing", "LIBRARY: typing — Support for type hints, derived from 'import typing', part of The Python Standard Library")
