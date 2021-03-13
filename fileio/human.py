01bef89f7e1f531473a2f421dd0efd19dedb1fc9
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 23:22:40 +0800

    Create human.py

---
+++ fileio/human.py
@@ -0,0 +1,6 @@
+## human
+from forest._importable import LazyImport
+
+
+pyyaml = LazyImport("import pyyaml", "PACKAGE pyyaml — The next generation YAML parser and emitter for Python, derived from 'import pyyaml', from https://github.com/yaml/pyyaml, Documentation: https://pyyaml.org/wiki/PyYAMLDocumentation")
+yaml = LazyImport("import pyyaml as yaml", "PACKAGE yaml derived from pyyaml — The next generation YAML parser and emitter for Python, derived from 'import pyyaml as yaml', from https://github.com/yaml/pyyaml, Documentation: https://pyyaml.org/wiki/PyYAMLDocumentation")
