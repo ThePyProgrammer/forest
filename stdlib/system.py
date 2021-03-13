abe4833ed8e2e3dc44aee27d2a0f2401ac012f46
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:30:58 +0800

    Create system.py

---
+++ stdlib/system.py
@@ -0,0 +1,7 @@
+## system
+sys = LazyImport("import sys", "LIBRARY: sys — System-specific parameters and functions, derived from 'import sys', part of The Python Standard Library")
+system = LazyImport("import sys as system", "LIBRARY: sys — System-specific parameters and functions, derived from 'import sys', part of The Python Standard Library")
+System = LazyImport("import sys as System", "LIBRARY: sys — System-specific parameters and functions, derived from 'import sys', part of The Python Standard Library")
+os = LazyImport("import os", "LIBRARY: os — Miscellaneous operating system interfaces, derived from 'import os', part of The Python Standard Library")
+stat = LazyImport("import stat", "LIBRARY: stat — Interpreting os.stat(), os.fstat() and os.lstat() results, derived from 'import stat', part of The Python Standard Library")
+subprocess = LazyImport("import subprocess", "LIBRARY: subprocess — Subprocess management, derived from 'import subprocess', part of The Python Standard Library")
