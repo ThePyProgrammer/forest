4ecf82ff9a33d7d592d36d0de40b05612b57fe0b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 23:13:50 +0800

    Update __init__.py

--- fileio/__init__.py
+++ fileio/__init__.py
@@ -5,11 +5,6 @@ from forest._importable import LazyImport
 io = LazyImport("import io", "LIBRARY: io — Core tools for working with streams, derived from 'import io', part of The Python Standard Library")
 fileinput = LazyImport("import fileinput", "LIBRARY fileinput — Iterate over lines from multiple input streams, derived from 'import fileinput', part of The Python Standard Library")
 
-## loc
-glob = LazyImport("import glob", "LIBRARY: glob — Unix style pathname pattern expansion, derived from 'import glob', part of The Python Standard Library")
-Path = LazyImport("from pathlib import Path", "CLASS: pathlib.Path — represents concrete paths of the system’s path flavour, derived from 'from pathlib import Path', part of The Python Standard Library")
-pathlib = LazyImport("import pathlib", "LIBRARY: pathlib — Object-oriented filesystem paths, derived from 'import pathlib', part of The Python Standard Library")
-
 
 pickle = LazyImport("import pickle", "LIBRARY: pickle — Python object serialization, derived from 'import pickle', part of The Python Standard Library")
 
