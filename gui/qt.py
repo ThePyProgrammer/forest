862d275738f9eb41ac702a15d2635653f37bbf5f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:30:43 +0800

    Create qt.py

---
+++ gui/qt.py
@@ -0,0 +1,7 @@
+## PyQT
+qt = LazyImport("import PyQt5 as qt", "PACKAGE qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as qt', from https://github.com/baoboa/pyqt5, Documentation: ")
+Qt = LazyImport("import PyQt5 as Qt", "PACKAGE Qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as Qt', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt = LazyImport("import PyQt5 as PyQt", "PACKAGE PyQt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as PyQt', from https://github.com/baoboa/pyqt5, Documentation: ")
+pyqt = LazyImport("import PyQt5 as pyqt", "PACKAGE pyqt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as pyqt', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt4 = LazyImport("import PyQt5 as PyQt4", "PACKAGE PyQt4 derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as PyQt5', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt5 = LazyImport("import PyQt5", "PACKAGE PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5', from https://github.com/baoboa/pyqt5, Documentation: ")
