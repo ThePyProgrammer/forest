0aedad4e6469a6110d33f32701952ae9766a36a8
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:30:36 +0800

    Create __init__.py

---
+++ gui/__init__.py
@@ -0,0 +1,27 @@
+# GUI
+
+## Tkinter
+tk = LazyImport("import tkinter as tk", "LIBRARY tk derived from tkinter — Python interface to Tcl/Tk, derived from 'import tkinter as tk', from The Python Standard Library")
+Tkinter = LazyImport("import tkinter as Tkinter", "LIBRARY Tkinter derived from tkinter — Python interface to Tcl/Tk, derived from 'import tkinter as Tkinter', from The Python Standard Library")
+tkinter = LazyImport("import tkinter", "LIBRARY tkinter — Python interface to Tcl/Tk, derived from 'import tkinter', from The Python Standard Library")
+tkFont = LazyImport("import tkinter.font as tkFont", "SUBLIBRARY tkFont derived from tkinter.font — Tkinter font wrapper, derived from 'import tkinter.font as tkFont', from The Python Standard Library")
+ttk = LazyImport("from tkinter import ttk", "SUBLIBRARY tkinter.ttk — Tk themed widgets, derived from 'from tkinter import ttk', from The Python Standard Library")
+turtle = LazyImport("import turtle", "LIBRARY turtle — Turtle graphics based on tkinter, derived from 'import turtle', from The Python Standard Library")
+teek = LazyImport("import teek", "PACKAGE teek — A pythonic and user-friendly alternative to tkinter, derived from 'import teek', from https://github.com/Akuli/teek, Documentation: https://teek.readthedocs.io/en/latest/")
+
+## PyQT
+qt = LazyImport("import PyQt5 as qt", "PACKAGE qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as qt', from https://github.com/baoboa/pyqt5, Documentation: ")
+Qt = LazyImport("import PyQt5 as Qt", "PACKAGE Qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as Qt', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt = LazyImport("import PyQt5 as PyQt", "PACKAGE PyQt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as PyQt', from https://github.com/baoboa/pyqt5, Documentation: ")
+pyqt = LazyImport("import PyQt5 as pyqt", "PACKAGE pyqt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as pyqt', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt4 = LazyImport("import PyQt5 as PyQt4", "PACKAGE PyQt4 derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as PyQt5', from https://github.com/baoboa/pyqt5, Documentation: ")
+PyQt5 = LazyImport("import PyQt5", "PACKAGE PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5', from https://github.com/baoboa/pyqt5, Documentation: ")
+
+## Pygame
+pygame = LazyImport("import pygame", "PACKAGE pygame — Free and Open Source library for making multimedia applications like games built on top of the excellent SDL library, derived from 'import pygame', from https://github.com/pygame/pygame, Documentation: https://www.pygame.org/docs/")
+mixer = LazyImport("from pygame import mixer", "SUBPACKAGE pygame.mixer — Pygame module for loading and playing sounds, derived from 'from pygame import mixer', from https://github.com/pygame/pygame, Documentation: https://www.pygame.org/docs/ref/mixer.html")
+
+## Kivy
+kivy = LazyImport("import kivy", "PACKAGE kivy — Open source UI framework written in Python, derived from 'import kivy', from https://github.com/kivy/kivy, Documentation: https://kivy.org/doc/stable/")
+kivyApp = LazyImport("from kivy.app import App as kivyApp", "CLASS kivyApp derived from kivy.app.App — Base for creating Kivy applications, derived from 'from kivy.app import App as kivyApp', from https://github.com/kivy/kivy, Documentation: https://kivy.org/doc/stable/api-kivy.app.html")
+kivyuilds = LazyImport("import kivy.uix as kivyuilds", "SUBPACKAGE kivyuilds derived from kivy.uix — Classes for creating and managing Widgets, derived from 'import kivy.uix as kivyuilds', from https://github.com/kivy/kivy, Documentation: https://kivy.org/doc/stable/api-kivy.uix.html")
