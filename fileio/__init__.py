5d54e1182f91fd73723ac8d710fcab44a49f2d66
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 23:14:40 +0800

    Update __init__.py

--- fileio/__init__.py
+++ fileio/__init__.py
@@ -28,4 +28,7 @@ sqlite3 = LazyImport("import sqlite3", "PACKAGE sqlite3 — An Embeddable SQL Da
 mysql = LazyImport("import sqlite3 as mysql", "PACKAGE mysql derived from sqlite3 — An Embeddable SQL Database Engine, derived from 'import sqlite3 as mysql', from http://www.sqlite.org/, Documentation: https://www.sqlite.org/docs.html")
 sql = LazyImport("import sqlite3 as sql", "PACKAGE sql derived from sqlite3 — An Embeddable SQL Database Engine, derived from 'import sqlite3 as sql', from http://www.sqlite.org/, Documentation: https://www.sqlite.org/docs.html")
 
+## xml
+lxml = LazyImport("import lxml", "PACKAGE lxml — The lxml XML toolkit for Python, derived from 'import lxml', from https://github.com/lxml/lxml, Documentation: https://lxml.de/")
+etree = LazyImport("from lxml import etree", "SUBPACKAGE lxml.etree — The ElementTree API, derived from 'from lxml import etree', from https://github.com/lxml/lxml, Documentation: https://lxml.de/")
 
