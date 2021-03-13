d6a4a05847ca9f4285506ee89ff500574899b760
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:18:14 +0800

    Update lazy.py

--- lazy.py
+++ lazy.py
@@ -37,6 +37,10 @@ from .mathematical import *
 from .array import *
 
 
+### String
+from .string import *
+
+
 ### Type based manipultation
 functools = LazyImport("import functools", "LIBRARY: functools — Higher-order functions and operations on callable objects, derived from 'import functools', part of The Python Standard Library")
 typing = LazyImport("import typing", "LIBRARY: typing — Support for type hints, derived from 'import typing', part of The Python Standard Library")
