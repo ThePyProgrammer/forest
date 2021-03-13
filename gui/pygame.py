4a529d7aa23cdcf1838aafd97b8e7fe9c013f6ad
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:30:46 +0800

    Create pygame.py

---
+++ gui/pygame.py
@@ -0,0 +1,3 @@
+## Pygame
+pygame = LazyImport("import pygame", "PACKAGE pygame — Free and Open Source library for making multimedia applications like games built on top of the excellent SDL library, derived from 'import pygame', from https://github.com/pygame/pygame, Documentation: https://www.pygame.org/docs/")
+mixer = LazyImport("from pygame import mixer", "SUBPACKAGE pygame.mixer — Pygame module for loading and playing sounds, derived from 'from pygame import mixer', from https://github.com/pygame/pygame, Documentation: https://www.pygame.org/docs/ref/mixer.html")
