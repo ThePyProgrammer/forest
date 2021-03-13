95af60786bf57e412f649b34bea9570c7026699e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:36:57 +0800

    Update __init__.py

--- mathematical/__init__.py
+++ mathematical/__init__.py
@@ -7,8 +7,8 @@ math = LazyImport("import math", "LIBRARY: math — Mathematical functions, deri
 ## complex math
 cmath = LazyImport("import cmath", "LIBRARY: cmath — Mathematical functions for complex numbers, derived from 'import cmath', part of The Python Standard Library")
 
-## statistics
-statistics = LazyImport("import statistics", "LIBRARY: statistics — Mathematical statistics functions, derived from 'import statistics', part of The Python Standard Library")
+## stats
+from .stats import *
 
 ## random
 from .rand import *
