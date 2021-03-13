35ece55213109de6b87d43b224759c445c228c42
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:00:14 +0800

    Update lazy.py

--- lazy.py
+++ lazy.py
@@ -36,16 +36,13 @@ from .mathematical import *
 ### Array
 
 
+
 ### Type based manipultation
-collections = LazyImport("import collections", "LIBRARY: collections — Container datatypes, derived from 'import collections', part of The Python Standard Library")
 functools = LazyImport("import functools", "LIBRARY: functools — Higher-order functions and operations on callable objects, derived from 'import functools', part of The Python Standard Library")
-itertools = LazyImport("import itertools", "LIBRARY: itertools — Functions creating iterators for efficient looping, derived from 'import itertools', part of The Python Standard Library")
 typing = LazyImport("import typing", "LIBRARY: typing — Support for type hints, derived from 'import typing', part of The Python Standard Library")
 textwrap = LazyImport("import textwrap", "LIBRARY: textwrap — Text wrapping and filling, derived from 'import textwrap', part of The Python Standard Library")
 stringprep = LazyImport("import stringprep", "LIBRARY: stringprep — Internet String Preparation, derived from 'import stringprep', part of The Python Standard Library")
 reprlib = LazyImport("import reprlib", "LIBRARY: reprlib — Alternate repr() implementation, derived from 'import reprlib', part of The Python Standard Library")
-arr = LazyImport("from array import array as arr", "CLASS arr derived from array.array — a base array, derived from 'from array import array as arr', part of The Python Standard Library")
-array_funcs = LazyImport("import array as array_funcs", "LIBRARY: array_funcs derived from array — Efficient arrays of numeric values, derived from 'import array as array_funcs', part of The Python Standard Library")
 
 
 ### Random Objects
