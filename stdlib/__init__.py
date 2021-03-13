669c9d502f3f311a0b0e82026d8fef7aeea72510
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:16:30 +0800

    Update __init__.py

--- stdlib/__init__.py
+++ stdlib/__init__.py
@@ -5,9 +5,6 @@ from forest._importable import LazyImport
 ## system
 from .system import *
 
-## string
-re = LazyImport("import re", "LIBRARY: re - regex operations, derived from 'import re', part of The Python Standard Library")
-
 ## io
 from .iopy import *
 
