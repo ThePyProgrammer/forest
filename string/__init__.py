b616e0a1322d3a605d8d5c2d2d93d25d428441d8
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 22:46:29 +0800

    Update __init__.py

--- string/__init__.py
+++ string/__init__.py
@@ -1,6 +1,7 @@
 # string
 from forest._importable import LazyImport
 
+
 textwrap = LazyImport("import textwrap", "LIBRARY: textwrap — Text wrapping and filling, derived from 'import textwrap', part of The Python Standard Library")
 stringprep = LazyImport("import stringprep", "LIBRARY: stringprep — Internet String Preparation, derived from 'import stringprep', part of The Python Standard Library")
 reprlib = LazyImport("import reprlib", "LIBRARY: reprlib — Alternate repr() implementation, derived from 'import reprlib', part of The Python Standard Library")
@@ -11,3 +12,6 @@ re = LazyImport("import re", "LIBRARY: re - regex operations, derived from 'impo
 ## unicode
 ucd = LazyImport("import unicodedata as ucd", "LIBRARY ucd derived from unicodedata — Unicode Database, derived from 'import unicodedata as ucd', part of The Python Standard Library")
 unicodedata = LazyImport("import unicodedata", "LIBRARY unicodedata — Unicode Database, derived from 'import unicodedata', part of The Python Standard Library")
+
+## print
+pprint = LazyImport("from pprint import pprint", "FUNCTION: pprint — Data pretty printer, derived from 'from pprint import pprint', part of The Python Standard Library")
