6d14996353ec6759936b8789c84d9545a6123975
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-13 21:46:00 +0800

    Update qt.py

--- gui/qt.py
+++ gui/qt.py
@@ -1,4 +1,6 @@
 ## PyQT
+from forest._importable import LazyImport
+
 qt = LazyImport("import PyQt5 as qt", "PACKAGE qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as qt', from https://github.com/baoboa/pyqt5, Documentation: ")
 Qt = LazyImport("import PyQt5 as Qt", "PACKAGE Qt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as Qt', from https://github.com/baoboa/pyqt5, Documentation: ")
 PyQt = LazyImport("import PyQt5 as PyQt", "PACKAGE PyQt derived from PyQt5 — Module that uses Qt from C++, derived from 'import PyQt5 as PyQt', from https://github.com/baoboa/pyqt5, Documentation: ")
