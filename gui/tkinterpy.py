812c7be3d05ccef4266022114a0ac69d638084df
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-14 16:39:43 +0800

    Update tkinterpy.py

--- gui/tkinterpy.py
+++ gui/tkinterpy.py
@@ -4,5 +4,13 @@ Tkinter = LazyImport("import tkinter as Tkinter", "LIBRARY Tkinter derived from 
 tkinter = LazyImport("import tkinter", "LIBRARY tkinter — Python interface to Tcl/Tk, derived from 'import tkinter', from The Python Standard Library")
 tkFont = LazyImport("import tkinter.font as tkFont", "SUBLIBRARY tkFont derived from tkinter.font — Tkinter font wrapper, derived from 'import tkinter.font as tkFont', from The Python Standard Library")
 ttk = LazyImport("from tkinter import ttk", "SUBLIBRARY tkinter.ttk — Tk themed widgets, derived from 'from tkinter import ttk', from The Python Standard Library")
+
+
 turtle = LazyImport("import turtle", "LIBRARY turtle — Turtle graphics based on tkinter, derived from 'import turtle', from The Python Standard Library")
+
+
 teek = LazyImport("import teek", "PACKAGE teek — A pythonic and user-friendly alternative to tkinter, derived from 'import teek', from https://github.com/Akuli/teek, Documentation: https://teek.readthedocs.io/en/latest/")
+
+
+tksheet = LazyImport("import tksheet")
+tkSheet = LazyImport("from tksheet import Sheet as tkSheet")
