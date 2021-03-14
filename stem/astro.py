6cdf087bb2497597a8379bf0e42715d7ffe9b551
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-14 16:46:28 +0800

    Update astro.py

--- stem/astro.py
+++ stem/astro.py
@@ -2,20 +2,22 @@
 from forest._importable import LazyImport
 
 
-scipy = LazyImport("import scipy", "PACKAGE scipy — open-source software for STEM, derived from 'import scipy', from https://github.com/scipy/scipy, Documentation: https://docs.scipy.org/doc/")
-#|----> Internal Scipy ----------------------------------------#
-sio = LazyImport("from scipy import io as sio")                #
-special = LazyImport("from scipy import special")              #
-fftpack = LazyImport("from scipy import fftpack")              #
-fft = LazyImport("from scipy import fft")                      #
-optimize = LazyImport("from scipy import optimize")            #
-op = LazyImport("from scipy import optimize as op")            #
-opt = LazyImport("from scipy import optimize as opt")          #
-ndimage = LazyImport("from scipy import ndimage")              #
-interpolate = LazyImport("from scipy import interpolate")      #
-stats = LazyImport("from scipy import stats")                  #
-signal = LazyImport("from scipy import signal")                #
-integrate = LazyImport("from scipy import integrate")          #
-csgraph = LazyImport("from scipy.sparse import csgraph")       #
-spatial = LazyImport("from scipy import spatial")              #
-#--------------------------------------------------------------#
+astropy = LazyImport("import astropy")
+
+sunpy = LazyImport("import sunpy")
+
+heliopy = LazyImport("import heliopy")
+ParkerSpiral = LazyImport("from heliopy import ParkerSpiral")
+
+galpy = LazyImport("import galpy")
+MiyamotoNagaiPotential = LazyImport("from galpy.potential import MiyamotoNagaiPotential")
+NFWPotential = LazyImport("from galpy.potential import NFWPotential")
+HernquistPotential = LazyImport("from galpy.potential import HernquistPotential")
+MWPotential = LazyImport("from galpy.potential import MWPotential")
+MWPotential2014 = LazyImport("from galpy.potential import MWPotential2014")
+Orbit = LazyImport("from galpy.orbit import Orbit")
+
+
+
+
+
