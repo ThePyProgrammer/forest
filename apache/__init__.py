7064d3cadeceba68518e0fc1afbd10a5118e4a38
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-14 13:46:37 +0800

    Create __init__.py

---
+++ apache/__init__.py
@@ -0,0 +1,8 @@
+# apache
+from ._importable import LazyImport, _import_statements
+
+
+pyspark = LazyImport("import pyspark", "PACKAGE pyspark — Python API for Apache Spark, derived from 'import pyspark', from https://github.com/apache/spark/tree/master/python, Documentation: https://spark.apache.org/docs/latest/api/python/pyspark.html")
+spark = LazyImport("import pyspark as spark", "PACKAGE spark derived from pyspark — Python API for Apache Spark, derived from 'import pyspark as spark', from https://github.com/apache/spark/tree/master/python, Documentation: https://spark.apache.org/docs/latest/api/python/pyspark.html")
+SparkContext = LazyImport("from pyspark import SparkContext", "CLASS pyspark.SparkContext — Main entry point for Spark functionality, derived from 'from pyspark import SparkContext', from https://github.com/apache/spark/tree/master/python, Documentation: https://spark.apache.org/docs/latest/api/python/pyspark.html#pyspark.SparkContext")
+
