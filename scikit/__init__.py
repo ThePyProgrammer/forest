f65b9f86c3a3dedeac807c5600c249a403f58de6
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-14 14:45:52 +0800

    Update __init__.py

--- scikit/__init__.py
+++ scikit/__init__.py
@@ -3,29 +3,56 @@ from ._importable import LazyImport, _import_statements
 
 
 sklearn = LazyImport("import sklearn", "PACKAGE sklearn — Machine learning in Python, derived from 'import sklearn', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
-OneHotEncoder = LazyImport("from sklearn.preprocessing import OneHotEncoder", "CLASS sklearn.preprocessing.OneHotEncoder — Encode categorical features as a one-hot numeric array, derived from 'from sklearn.preprocessing import OneHotEncoder', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
-# , "CLASS sklearn. — , derived from 'from sklearn. import ', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
+
+
 train_test_split = LazyImport("from sklearn.model_selection import train_test_split" , "FUNCTION sklearn.model_selection.train_test_split — Split arrays or matrices into random train and test subsets, derived from 'from sklearn.model_selection import train_test_split', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
 cross_val_score = LazyImport("from sklearn.model_selection import cross_val_score" , "FUNCTION sklearn.model_selection.cross_val_score — Evaluate a score by cross validation, derived from 'from sklearn.model_selection import cross_val_score', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
 StratifiedKFold = LazyImport("from sklearn.model_selection import StratifiedKFold" , "CLASS sklearn.model_selection.StratifiedKFold — Stratified K-Folds cross-validator, derived from 'from sklearn.model_selection import StratifiedKFold', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
+
+
 LogisticRegression = LazyImport("from sklearn.linear_model import LogisticRegression")
+
+
 DecisionTreeClassifier = LazyImport("from sklearn.tree import DecisionTreeClassifier")
+
+
 KNeighborsClassifier = LazyImport("from sklearn.neighbours import KNeighborsClassifier")
+
+
 LinearDiscriminantAnalysis = LazyImport("from sklearn.discriminant_analysis import LinearDiscriminantAnalysis")
+
+
 GaussianNB = LazyImport("from sklearn.naive_bayes import GaussianNB")
+
+
 classification_report = LazyImport("from sklearn.metrics import classification_report")
 confusion_matrix = LazyImport("from sklearn.metrics import confusion_matrix")
 accuracy_score = LazyImport("from sklearn.metrics import accuracy_score")
 make_scorer = LazyImport("from sklearn.metrics import make_scorer")
+
+
 TSNE = LazyImport("from sklearn.manifold import TSNE")
+
+
 svm = LazyImport("from sklearn import svm")
 SVC = LazyImport("from sklearn.svm import SVC")
+
+
 make_pipeline = LazyImport("from sklearn.pipeline import make_pipeline")
+
+
+OneHotEncoder = LazyImport("from sklearn.preprocessing import OneHotEncoder", "CLASS sklearn.preprocessing.OneHotEncoder — Encode categorical features as a one-hot numeric array, derived from 'from sklearn.preprocessing import OneHotEncoder', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
+
+
 GradientBoostingClassifier = LazyImport("from sklearn.ensemble import GradientBoostingClassifier")
 GradientBoostingRegressor = LazyImport("from sklearn.ensemble import GradientBoostingRegressor")
 RandomForestClassifier = LazyImport("from sklearn.ensemble import RandomForestClassifier")
 RandomForestRegressor = LazyImport("from sklearn.ensemble import RandomForestRegressor")
+
+
 TfidfVectorizer = LazyImport("from sklearn.feature_extraction.text import TfidfVectorizer")
+
+
 datasets = LazyImport("from sklearn import datasets")
 load_iris = LazyImport("from sklearn.datasets import load_iris")
 Iris = LazyImport("from sklearn.datasets import load_iris as Iris")
@@ -38,9 +65,6 @@ Boston = LazyImport("from sklearn.datasets import load_boston as Boston")
 load_breast_cancer = LazyImport("from sklearn.datasets import load_breast_cancer")
 BreastCancer = LazyImport("from sklearn.datasets import load_breast_cancer as BreastCancer")
 
-
-
-
 make_biclusters = LazyImport("from sklearn.datasets import make_biclusters")
 make_blobs = LazyImport("from sklearn.datasets import make_blobs")
 make_checkerboard = LazyImport("from sklearn.datasets import make_checkerboard")
@@ -48,8 +72,4 @@ make_circles = LazyImport("from sklearn.datasets import make_circles")
 make_classification = LazyImport("from sklearn.datasets import make_classification")
 make_regression = LazyImport("from sklearn.datasets import make_regression")
 
-#diabetes = load_diabetes()
-#digits = load_digits()
-#iris = load_iris()
-#iris_df = pd.DataFrame(iris.data, columns = iris.feature_names)
-#diabetes_df = pd.DataFrame(diabetes.data, columns = diabetes.feature_names)
+# , "CLASS sklearn. — , derived from 'from sklearn. import ', from https://github.com/scikit-learn/scikit-learn, Documentation: https://scikit-learn.org/stable/modules/classes.html")
